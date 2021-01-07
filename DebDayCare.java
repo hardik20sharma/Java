389186abe87c31235815f66b9cf01f12a2d13e5a
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2021-01-07 18:01:41 +0530

    Update DebDayCare.java

--- DebDayCare.java
+++ DebDayCare.java
@@ -17,21 +17,21 @@ public class DebDayCare
 	
 	public void set_Details(int id, String name, int employee_code, int no_of_children, int no_of_hours, int wage)
 	{
-		this.id 			= id;
-		this.name 			= name;
-		this.employee_code 	= employee_code;
+		this.id = id;
+		this.name = name;
+		this.employee_code = employee_code;
 		this.no_of_children = no_of_children;
-		this.no_of_hours 	= no_of_hours;
-		this.wage 			= wage;
+		this.no_of_hours = no_of_hours;
+		this.wage = wage;
 	}
 	
 	public void display_Details()
 	{
-		System.out.println("ID:" 				+ id);
-		System.out.println("Name:" 				+ name);
-		System.out.println("Employee Code:" 	+ employee_code);
-		System.out.println("No. of children:" 	+ no_of_children);
-		System.out.println("No. of hours:" 		+ no_of_hours);
-		System.out.println("Wage:" 				+ wage);
+		System.out.println("ID:" + id);
+		System.out.println("Name:" + name);
+		System.out.println("Employee Code:" + employee_code);
+		System.out.println("No. of children:" + no_of_children);
+		System.out.println("No. of hours:" + no_of_hours);
+		System.out.println("Wage:" + wage);
 	}
 }
